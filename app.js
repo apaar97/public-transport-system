385437be3c2f40968b5dcc2a5c1b7d44789d9644
Aditya <aditya13197@gmail.com>
2018-04-11 12:53:21 +0530

    basic setup

---
+++ app.js
@@ -0,0 +1,13 @@
+var express =  require('express');
+// Init app
+const app = express();
+
+
+app.get('/',(req,res)=>{res.send("Aakash")});
+
+// Start the server
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
